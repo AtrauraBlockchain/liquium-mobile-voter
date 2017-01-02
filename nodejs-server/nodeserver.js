5d1656430acbbbf7a3f05fa19cb174e56ea76e5f
Jordi Baylina <jordi@baylina.cat>
2017-01-02 09:57:52 +0100

    Contracts ported to other repository

--- nodejs-server/nodeserver.js
+++ nodejs-server/nodeserver.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 var port = 8080;
 
-var polls = require('./polls/pollsmock.js');
+var ethConnector = require('ethconnector');
+var liquiumRT = require('../contracts/js/liquium_rt.js');
 
 
 app.all('*', function(req, res, next) {
@@ -10,15 +11,38 @@ app.all('*', function(req, res, next) {
     next();
 });
 
-app.get('/api/polls', function (req, res) {
-    res.end(polls.generatePollsJson());
+app.get('/api/organization/:idOrganization/polls', function (req, res, next) {
+    liquiumRT.getPolls(req.params.idOrganization, function(err, polls) {
+        if (err) return next(err);
+        res.json(polls);
+    });
+});
+
+api.post('/api/organizations', function(req, res, next) {
+    liquiumRT.deployOrganization({}, function(err, organization) {
+        if (err) return next(err);
+        res.json({ organizationAddr: organization.address });
+    });
+});
+
+api.post('/api/organization/:idOrganization/polls', function(req, res, next) {
+
 });
 
 app.get('*', function(req, res) {
     res.end('404');
 });
 
-var server = app.listen(port, function() {
-    var port = server.address().port;
-    console.log('server listening at http://localhost:' + port.toString());
+var server;
+
+ethConnector.init('rpc' ,{gasLimit: 4700000},function(err) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    } else {
+        server = app.listen(port, function() {
+            var port = server.address().port;
+            console.log('server listening at http://localhost:' + port.toString());
+        });
+    }
 });
---
+++ nodejs-server/polls/polls.js
@@ -0,0 +1,13 @@
+/*jslint node: true */
+"use strict";
+
+
+
+
+
+
+function getPolls(organizationAddr) {
+
+    web3.eth.contract()
+
+}
